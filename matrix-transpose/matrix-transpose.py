0c00f9f65c2e4d39f67f745bbe8344e75ce2bb65
Vivek <152723348+VivekShinde0013@users.noreply.github.com>
2026-04-14 23:48:57 +0530

    Implement matrix transpose function
    Removed docstring and added code to create a transposed matrix.

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -1,9 +1,7 @@
 import numpy as np
 
 def matrix_transpose(A):
-    """
-    Return the transpose of matrix A (swap rows and columns).
-    """
+
     # Write code here
     A=np.array(A)
     rows, cols= A.shape
